4cd8edc6e26bc3d440b3f95ddd1b12979ddc6110
Matt <11328975+zSoulweaver@users.noreply.github.com>
2023-07-18 21:58:37 +1000

    docs: add information on entry class

--- src/client/kient.ts
+++ src/client/kient.ts
@@ -2,6 +2,8 @@ import { ChannelEndpoint } from '../endpoints/channel/channel.endpoint'
 import { ApiClient } from './api-client'
 
 /**
+ * Entry class for interacting with Kick's API and WebSocket.
+ *
  * @class Kient
  */
 export class Kient {
@@ -16,6 +18,14 @@ export class Kient {
   }
 
   /**
+   * Returns object of available endpoint categories that can be consumed.
+   *
+   * @Example
+   * ```ts
+   * const kient = new Kient()
+   * const channelBans = await kient.api().channel.getBans('example')
+   * ```
+   *
    * @readonly
    */
   api() {
