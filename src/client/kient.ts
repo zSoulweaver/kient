d44c0f0dcf2db9aef25fc7523b22cc02b1f4a7b6
Matt <11328975+zSoulweaver@users.noreply.github.com>
2023-11-09 19:49:19 +1000

    chore: remove old jsdocs

--- src/client/kient.ts
+++ src/client/kient.ts
@@ -5,21 +5,10 @@ export interface Endpoints {
   channel: ChannelEndpoint
 }
 
-/**
- * Entry class for interacting with Kick's API and WebSocket.
- *
- * @class Kient
- */
 export class Kient {
-  /** @hidden */
   private _apiClient: ApiClient
-  /** @hidden */
   private _endpoints: Endpoints
 
-  /** @hidden */
-  constructor() { }
-
-  /** @hidden */
   private async init() {
     this._apiClient = await ApiClient.create(this)
     this._endpoints = {
@@ -27,37 +16,12 @@ export class Kient {
     }
   }
 
-  /**
-   * Creates a new instance of Kient
-   *
-   * @example
-   * ```ts
-   * import { Kient } from 'kient'
-   *
-   * const kient = await Kient.create()
-   * ```
-   *
-   * @static
-   * @returns Kient
-   */
   static async create() {
     const kient = new Kient()
     await kient.init()
     return kient
   }
 
-  /**
-   * Returns object of available endpoint categories that can be consumed.
-   *
-   * @Example
-   * ```ts
-   * const kient = new Kient()
-   * const channelBans = await kient.api.channel.getChannel('xqc')
-   * ```
-   *
-   * @readonly
-   * @returns Endpoints
-   */
   get api() {
     return this._endpoints
   }
