10fff0cadb8ce298bd8532595b0539e74ebf9efa
Matt <11328975+zSoulweaver@users.noreply.github.com>
2023-12-03 21:03:32 +1000

    docs: add frontmatter plugin

--- typedoc-plugin.js
+++ typedoc-plugin.js
@@ -1,7 +1,33 @@
 import * as path from 'node:path'
 import * as fs from 'node:fs'
+import { FrontmatterEvent } from 'typedoc-plugin-frontmatter'
+import { PageEvent } from 'typedoc'
 
 export function load(app) {
+  const outlines = []
+
+  app.renderer.on(
+    FrontmatterEvent.PREPARE_FRONTMATTER,
+    (event) => {
+      if (event.frontmatter.outline) {
+        outlines.push(event.page.model.url)
+        if (event.frontmatter.outline !== 'deep')
+          event.frontmatter.outline = JSON.parse(event.frontmatter.outline)
+      }
+    },
+  )
+
+  app.renderer.on(PageEvent.END, (page) => {
+    if (page.contents && outlines.includes(page.model.url) && page.contents.includes('## Outline')) {
+      const startIndex = page.contents.indexOf('## Outline')
+      const nextHeadingIndex = page.contents.indexOf('##', startIndex + 1)
+
+      page.contents = nextHeadingIndex !== -1
+        ? page.contents.substring(0, startIndex) + page.contents.substring(nextHeadingIndex)
+        : page.contents.substring(0, startIndex)
+    }
+  })
+
   app.renderer.postRenderAsyncJobs.push(
     (output) => {
       const outDir = app.options.getValue('out')
