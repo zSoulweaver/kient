c571a1e6d405970b5f2d790b63724260cd655f7b
Matt <11328975+zSoulweaver@users.noreply.github.com>
2023-12-05 21:15:00 +1000

    chore: remove explicit-member-accessibility rule

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,5 @@ import antfu from '@antfu/eslint-config'
 export default antfu({
   rules: {
     'style/brace-style': ['error', '1tbs'],
-    'ts/explicit-member-accessibility': ['error'],
   },
 })
