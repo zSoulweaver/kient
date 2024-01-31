9e3e087ce7c110255bf3eeef8faa3d99c7a91709
Matt <11328975+zSoulweaver@users.noreply.github.com>
2024-01-31 21:35:23 +1000

    refactor: remove redundant property

--- src/ws/chatroom/instance/chat-message.instance.ts
+++ src/ws/chatroom/instance/chat-message.instance.ts
@@ -5,8 +5,6 @@ import { ChatMessageEvent, ChatterStatus } from '../dto/chat-message.event'
  * @category Instances
  */
 export class ChatMessageInstance extends ChatroomBaseInstance<ChatMessageEvent> {
-  chatroomId: string
-
   public chatterIs(chatterStatus: typeof ChatterStatus | string) {
     const badges = this.data.sender.identity.badges
     return badges.filter(x => x.type === chatterStatus).length > 0
