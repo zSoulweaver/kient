3dc81461d5470176f907444ebb2c42ae99a05130
zSoulweaver <11328975+zSoulweaver@users.noreply.github.com>
2024-07-30 21:15:44 +1000

    feat: add channel panels

---
+++ src/api/channels/get-panels.response.ts
@@ -0,0 +1,6 @@
+// biome-ignore lint/style/useImportType: deepkit/type
+import { Panel } from '../../structures/panel'
+
+export interface ChannelPanelsResponse {
+	panels: Panel[]
+}
---
+++ src/api/channels/get-panels.ts
@@ -0,0 +1,22 @@
+import { cast } from '@deepkit/type'
+import type { Kient } from '../../kient'
+// biome-ignore lint/style/useImportType: deepkit/type
+import { CommonAPIResponse, isSuccessResponse } from '../../util/common-api-response'
+// biome-ignore lint/style/useImportType: deepkit/type
+import { ChannelPanelsResponse } from './get-panels.response'
+// biome-ignore lint/style/useImportType: deepkit/type
+import { Panel } from '../../structures/panel'
+
+export async function getPanels(kient: Kient, slugOrId: string) {
+	const response = await kient._apiClient.fetch<CommonAPIResponse<ChannelPanelsResponse>>(
+		`/channels/${slugOrId}/panels`,
+	)
+
+	const typedResponse = cast<CommonAPIResponse<ChannelPanelsResponse>>(response)
+	if (!isSuccessResponse(typedResponse)) {
+		throw new Error(`Request failed: ${typedResponse.data}`)
+	}
+
+	const panels = cast<Panel[]>({ ...typedResponse.data.panels, kient })
+	return panels
+}
