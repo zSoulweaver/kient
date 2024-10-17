9f67eb29c1346f284e288bcf4030b6574cb005ea
bananacity <72461057+bananacity@users.noreply.github.com>
2024-10-17 15:34:49 +0100

    Update src/core/api.client.ts
    Co-authored-by: zSoulweaver <11328975+zSoulweaver@users.noreply.github.com>

--- src/core/api.client.ts
+++ src/core/api.client.ts
@@ -54,6 +54,7 @@ export class ApiClient {
       ja3: '771,4865-4867-4866-49195-49199-52393-52392-49196-49200-49162-49161-49171-49172-51-57-47-53-10,0-23-65281-10-11-35-16-5-51-43-13-45-28-21,29-23-24-25-256-257,0',
       userAgent: 'KICK/1.0.13 Dalvik/2.1.0(Linux; U; Android 13; Pixel 6 Pro Build / TQ1A.221205.011)',
       ...(this.proxy && {proxy: this.proxy}),
+      ...(this.proxy && { proxy: this.proxy }),
       headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
