d0a2423c54efead6526db04ae44f0d9169f4deb9
zSoulweaver <11328975+zSoulweaver@users.noreply.github.com>
2025-02-21 18:06:58 +1000

    refactor: remove old examples

--- example/get-channel.ts
+++
@@ -1,8 +0,0 @@
-import { inspect } from 'bun'
-import { Kient } from 'kient'
-
-const kient = new Kient()
-const res = await kient.api.channel.get('channel_01GFZ7V9JR2H2AM2B3ZP7EDVKF')
-
-console.log(res.channel.toJSON())
-console.log(res.user.toJSON())
