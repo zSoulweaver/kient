91667c1255e55f9fecb2031ea468ae6b83ed3be7
Kody <gh@kdy.ch>
2025-04-09 11:54:35 +0200

    feat: support getting channels by slug (#32)

--- src/api/channel/index.ts
+++ src/api/channel/index.ts
@@ -1,5 +1,5 @@
 import { APIBase } from '../api-base'
-import { getChannelsByID } from './get-channels'
+import { getChannelsByID, getChannelsBySlugs } from './get-channels'
 
 /**
  * Description placeholder
@@ -23,6 +23,24 @@ export class ChannelAPI extends APIBase {
 	 */
 	async getById(id: number) {
 		return (await getChannelsByID(this.kient, [id]))[0]
+	}
+
+	/**
+	 * Returns an array of channel by an array of channel slugs
+	 *
+	 * @param slugs Accepts an array of channel slugs that will be queried for
+	 */
+	getBySlugs(slugs: string[]) {
+		return getChannelsBySlugs(this.kient, slugs)
+	}
+
+	/**
+	 * Returns a singular channel by slug
+	 *
+	 * @param slug Accepts a singular channel slug that will be queried for
+	 */
+	async getBySlug(slug: string) {
+		return (await getChannelsBySlugs(this.kient, [slug]))[0]
 	}
 
 	/**
