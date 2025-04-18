6a72f7be78a28154f6411fc41bbd723d20fe215b
zSoulweaver <11328975+zSoulweaver@users.noreply.github.com>
2025-04-18 14:36:09 +1000

    chore: update webhook example - data structures should really be use `number` type, will get on that later

--- example/webhooks.ts
+++ example/webhooks.ts
@@ -11,10 +11,16 @@ if (kient.webhookServerFetch) {
 	})
 }
 
+const specificUserBySlug = await kient.api.channel.getBySlug('kient')
+
 const subscription = await kient.api.event.subscribe({
 	method: 'webhook',
-	broadcaster_user_id: 676,
+	broadcaster_user_id: Number.parseInt(specificUserBySlug.id),
 	events: [
+		{
+			name: 'livestream.status.updated',
+			version: 1,
+		},
 		{
 			name: 'chat.message.sent',
 			version: 1,
@@ -25,9 +31,13 @@ console.log(kientToJSON(subscription))
 
 const subscriptions = await kient.api.event.getSubscriptions()
 console.log(subscriptions.map((sub) => sub.toJSON()))
-// subscriptions.forEach(sub => sub.unsubscribe())
+// subscriptions.forEach((sub) => sub.unsubscribe())
 
 kient.addListener('KIENT_CHAT_MESSAGE_SENT', (message) => {
 	console.log('chat message received from', message.sender.username)
+	console.log(kientToJSON(message))
+})
+
+kient.addListener('KIENT_LIVESTREAM_STATUS_UPDATED', (message) => {
 	console.log(kientToJSON(message))
 })
