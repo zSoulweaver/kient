d6b305649af5c464f03b4e20e6128c44b3ffd188
zSoulweaver <11328975+zSoulweaver@users.noreply.github.com>
2025-04-18 14:24:28 +1000

    feat: add chat user identity

--- src/structures/chat-user.ts
+++ src/structures/chat-user.ts
@@ -9,6 +9,14 @@ export interface ChatUserData {
 	is_verified: boolean
 	profile_picture: string
 	channel_slug: string
+	identity?: {
+		username_color: string
+		badges: Array<{
+			text: string
+			type: string
+			count: number
+		}>
+	}
 }
 
 export class ChatUser extends BaseUser {
@@ -22,12 +30,48 @@ export class ChatUser extends BaseUser {
 	 */
 	slug: string
 
+	/**
+	 * The user's chat appearance
+	 */
+	identity?: {
+		/**
+		 * The user's color in chat
+		 */
+		usernameColor: string
+
+		/**
+		 * The user's acquired chat badges
+		 */
+		badges: Array<{
+			/**
+			 * The badge's display name
+			 */
+			text: string
+
+			/**
+			 * The badge's identifier
+			 */
+			type: string
+
+			/**
+			 *  How many of this badge the user has
+			 * 	Generally related to how many months a user has subscribed for
+			 *
+			 * 	TODO: This could use better clarification
+			 */
+			count?: number
+		}>
+	}
+
 	/** @internal */
 	constructor(kient: Kient, data: Flattened<ChatUser>) {
 		super(kient, data)
 
 		this.isVerified = data.isVerified
 		this.slug = data.slug
+		if (data.identity) {
+			this.identity = data.identity
+		}
 	}
 
 	static constructChatUser(data: ChatUserData): Flattened<ChatUser> {
@@ -37,6 +81,12 @@ export class ChatUser extends BaseUser {
 			profilePicture: data.profile_picture,
 			isVerified: data.is_verified,
 			slug: data.channel_slug,
+			identity: data.identity
+				? {
+						usernameColor: data.identity?.username_color,
+						badges: data.identity?.badges,
+					}
+				: undefined,
 		}
 	}
 
